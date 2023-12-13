d6456248df2a93bb1040fd8f5509540c88fee149
Jits <dev@jits.co.uk>
2023-12-13 12:56:49 +0000

    Custom Firebase set up with common code

---
+++ firebase/common/index.ts
@@ -0,0 +1,3 @@
+export type Test = {
+  foo: string;
+};
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,8 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+import { Test } from '../../common';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +19,5 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const test: Test = { foo: 'bar' };
